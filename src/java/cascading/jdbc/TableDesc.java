2d689e2b6a08a9a57613c1ea024c128d9c34140d
Chris K Wensel <chris@wensel.net>
2009-02-05 17:55:11 -0800

    updated copyright notice

--- src/java/cascading/jdbc/TableDesc.java
+++ src/java/cascading/jdbc/TableDesc.java
@@ -1,5 +1,13 @@
 /*
- * Copyright (c) 2009, Your Corporation. All Rights Reserved.
+ * Copyright (c) 2009 Concurrent, Inc.
+ *
+ * This work has been released into the public domain
+ * by the copyright holder. This applies worldwide.
+ *
+ * In case this is not legally possible:
+ * The copyright holder grants any entity the right
+ * to use this work for any purpose, without any
+ * conditions, unless such conditions are required by law.
  */
 
 package cascading.jdbc;
